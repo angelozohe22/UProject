b020445a1d4d3204cd95fc109dd1682db3265b84
Luis Angel Inga Mendoza <luis_angelo21@hotmail.com>
2022-04-30 02:52:38 -0500

    fix google service pluggin and factory to instance repository

--- app/src/main/java/com/example/uproject/ui/viewmodels/auth/factory/AuthViewModelFactory.kt
+++ app/src/main/java/com/example/uproject/ui/viewmodels/auth/factory/AuthViewModelFactory.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.uproject.domain.auth.AuthRepository
 
-class AuthViewModelFactory(private val repository: AuthRepository): ViewModelProvider.Factory{
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+class AuthViewModelFactory (private val repository: AuthRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(AuthRepository::class.java).newInstance(repository)
     }
 }
--- app/src/main/java/com/example/uproject/ui/viewmodels/home/factoryhome/HomeViewModelFactory.kt
+++ app/src/main/java/com/example/uproject/ui/viewmodels/home/factoryhome/HomeViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.uproject.domain.repository.DulcekatRepository
 
 class HomeViewModelFactory(private val repository: DulcekatRepository): ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(DulcekatRepository::class.java).newInstance(repository)
     }
 }
